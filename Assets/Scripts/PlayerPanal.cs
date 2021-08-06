5daff93d7b6e11524dfad3d3e61c9bec17e27b60
Anthony Ruffalo <antruffii@gmail.com>
2021-08-05 22:21:09 -0500

    Created Scripts to allow for dymanic player panals.

---
+++ Assets/Scripts/PlayerPanal.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPanal : MonoBehaviour
+{
+	public Image image;
+	public Sprite portrait;
+	public Text playerText;
+	public string playerName;
+	public Text heartText;
+	public int heartCounter;
+	public Text soulsText;
+	public int soulsCounter;
+	public Text humanityText;
+	public int humanityCounter;
+
+
+  void Start()
+  {
+		image.sprite = portrait;
+		playerText.text = playerName;
+		heartText.text = "Hearts: " + heartCounter.ToString();
+		soulsText.text = "Souls: " + soulsCounter.ToString();
+		humanityText.text = "Humanity: " + humanityCounter.ToString();
+  }
+
+	public void changeHearts(int value)
+	{
+		heartCounter += value;
+		heartText.text = "Hearts: " + heartCounter.ToString();
+	}
+
+	public void changeSouls(int value)
+	{
+		soulsCounter += value;
+		soulsText.text = "Souls: " + soulsCounter.ToString();
+	}
+
+	public void changeHumanity(int value)
+	{
+		humanityCounter += value;
+		humanityText.text = "Souls: " + humanityCounter.ToString();
+	}
+	// Update is called once per frame
+	void Update()
+  {
+        
+  }
+}
